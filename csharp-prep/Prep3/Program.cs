36205bf30b7c7d4c1161e87e02df15433a50feec
Hector Rodriguez <rodriguezhr25@gmail.com>
2023-09-16 23:34:43 -0400

    prep4 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        string option ="";
+        int guesses = 0;
+        do
+        {
+            bool result = true;
+            string textResult;
+            string guessInput;
+            int guessNumber;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);       
+           
+        
+            do
+            {
+
+                Console.Write("What is your guess? ");
+                guessInput = Console.ReadLine();
+                guessNumber = int.Parse(guessInput);
+
+                if (guessNumber < magicNumber)
+                {
+                    textResult = "Higher";
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    textResult = "Lower";
+                }
+                else
+                {
+                    textResult = "You guessed it!";
+                    result = false;
+                    guesses++;
+                }
+                
+                Console.WriteLine(textResult);
+            } while (result);
+            
+            Console.WriteLine("Game Over");
+            Console.Write("Do you want to play again? ");
+            option = Console.ReadLine();
+        } while (option == "yes");
+        Console.WriteLine($"You got {guesses} guesses! ");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,62 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string userInput;
+        int numberInput;
+        List<int> numbersList = new List<int>();
+        List<int> postiveNumbersList = new List<int>();
+        int sum = 0;
+        float average;
+
+        Console.WriteLine("Enter a list of numbers (negative or positive), type 0 when finished. ");
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            numberInput = int.Parse(userInput);
+            if (numberInput != 0)
+            {
+                numbersList.Add(numberInput);
+            }
+
+
+        } while (numberInput != 0);
+
+        // Sum
+        foreach (int number in numbersList)
+        {
+            sum += number;
+        }
+        // average
+        average = (float)(sum) / numbersList.Count();
+        // Get Max value
+        int max = numbersList.Max();
+
+        // get  smallest positive value
+        //First we copy the list and then we remove all negative values
+        foreach (int number in numbersList)
+        {
+           postiveNumbersList.Add(number);
+        }
+        postiveNumbersList.RemoveAll(t => t < 0);
+        postiveNumbersList.Sort();
+        int minPositive = postiveNumbersList.Min();
+        
+
+        //Print results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number: {max}");
+        Console.WriteLine($"The smallest positive number is: {minPositive}");
+        Console.WriteLine("The sorted list is:");
+
+        numbersList.Sort();
+        foreach (int number in numbersList)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
